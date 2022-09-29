5f740b612b0ce3e6a17116211b3997eb7fd01acb
Hanzhi <Hanzhi100@outlook.com>
2022-09-28 21:43:16 -0400

    add social icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
 function App() {
   return (
     <div>
       <h1>
         <Navbar />
+        <Home />
       </h1>
     </div>
   );
